d35f67063ba469400f1844dd09c4fc5702838797
topether21 <topether21@gmail.com>
2023-03-23 12:28:46 -0600

    wip: add bitcoinjs-lib

--- src/crypto.ts
+++ src/crypto.ts
@@ -2,10 +2,10 @@ import * as bitcoin from 'bitcoinjs-lib';
 import { TESTNET } from './constants';
 
 export const getAddress = (nostrPublicKey: string) => {
-    const pubkeyBuffer = Buffer.from(nostrPublicKey, 'hex');
-    const addrInfo = bitcoin.payments.p2tr({
-        pubkey: pubkeyBuffer,
-        network: TESTNET ? bitcoin.networks.testnet : bitcoin.networks.bitcoin,
-    });
-    return addrInfo;
+  const pubkeyBuffer = Buffer.from(nostrPublicKey, 'hex');
+  const addrInfo = bitcoin.payments.p2tr({
+    pubkey: pubkeyBuffer,
+    network: TESTNET ? bitcoin.networks.testnet : bitcoin.networks.bitcoin,
+  });
+  return addrInfo;
 };
